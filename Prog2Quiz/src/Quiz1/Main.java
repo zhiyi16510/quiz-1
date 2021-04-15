244ae3f9079c8abd34dbc493be3ecca6ad593732
TEOH ZHI YI 278645 <73087918+zhiyi16510@users.noreply.github.com>
2021-04-15 13:57:48 +0800

    Update Main.java

--- Prog2Quiz/src/Quiz1/Main.java
+++ Prog2Quiz/src/Quiz1/Main.java
@@ -11,12 +11,13 @@ public class Main {
 		iphone.setYear();
 		iphone.setPrice();
 		
+		System.out.println();
 		System.out.println(" ******* IPHONE ******* ");
 		System.out.println("The brand is: " +iphone.getBrand());
 		System.out.println("The model is: " +iphone.getModel());
 		System.out.println("The operating system is: " +iphone.getOS());
 		System.out.println("The released year is: " +iphone.getYear());
-		System.out.println("The price is: " +iphone.getPrice());
+		System.out.println("The price is: RM" +iphone.getPrice());
 	
 		
 		
